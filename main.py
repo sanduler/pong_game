be0e503ae63e610458deb25d83bbd56d52786c06
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-03 09:23:41 -0800

    OOP - Paddle class, and added second paddle

--- main.py
+++ main.py
@@ -2,14 +2,13 @@
 # Date: February 3rd, 2022
 # Description: Pong game built with OOP in mind.
 
-import random
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 GAME_TITLE = "Pong"
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 BACKGROUND_COLOR = "Black"
-
 
 screen = Screen()
 screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
@@ -17,39 +16,17 @@ screen.bgcolor(BACKGROUND_COLOR)
 screen.title(GAME_TITLE)
 screen.tracer(0)
 
-paddle = Turtle(shape="square")
-paddle.color("White")
-paddle.resizemode("auto")
-paddle.turtlesize(stretch_wid=5, stretch_len=1)
-
-paddle.penup()
-paddle.goto((350, 0))
-
-
-def paddle_up():
-    new_y = paddle.ycor() + 20
-    paddle.goto(paddle.xcor(), new_y)
-
-
-def paddle_down():
-    new_y = paddle.ycor() - 20
-    paddle.goto(paddle.xcor(), new_y)
-
+right_paddle = Paddle((350, 0))
+left_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(paddle_up, "Up")
-screen.onkey(paddle_down, "Down")
-
+screen.onkey(right_paddle.paddle_up, "Up")
+screen.onkey(right_paddle.paddle_down, "Down")
+screen.onkey(left_paddle.paddle_up, "w")
+screen.onkey(left_paddle.paddle_down, "s")
 
 game_is_on = True
 while game_is_on:
     screen.update()
-
-
-
-
-
-
-
 
 screen.exitonclick()
